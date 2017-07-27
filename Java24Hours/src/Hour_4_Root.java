7dcd8e479b8778ab32eda1236c4c6040d03a3661
Yash Malik <ymalik2000@gmail.com>
2017-07-26 20:09:56 -0400

    Add files via upload

---
+++ Java24Hours/src/Hour_4_Root.java
@@ -0,0 +1,10 @@
+public class Hour_4_Root {
+    public static void main(String[] args){
+        int number = 225;
+        System.out.println("The quare root of "
+        + number
+        + " is "
+        +Math.sqrt(number)
+        );
+    }
+}
